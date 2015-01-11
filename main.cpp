f3b92ce4f140a61d02ad5165f4ff79d654af3a1b
nomura <riscster@gmail.com>
2015-01-12 03:33:05 +0900

    commit

--- main.cpp
+++ main.cpp
@@ -1,17 +1,32 @@
 //
-//  main
+//  main.cpp
 //  lynx
 //
 //  Created by nomura 15/1/8
 //  Copyright(c) 2015 nomura. All rights reversed.
 
+/* **** */
+/* MEMO */
+/* **** */
+// _WIN32                 ：Windows(32bit/64bit)の場合に定義される
+// _WIN64                 ：Windows(64bit)の場合に定義される
+// __APPLE__              ：MacOS X / iOS(実機 / シミュレータ）の場合に定義される
+// TARGET_OS_IPHONE       ：iOS(実機 / シミュレータ）の場合に定義される
+// TARGET_IPHONE_SIMULATOR：iOS（シミュレータ）の場合に定義される
+
 #ifdef _WIN32
-#include<windows.h>
-#else
+#  include<windows.h>
+#elif __APPLE__
+#  ifdef TARGET_OS_IPHONE
+#    import <UIKit/UIKit.h>
+#  else
+#    import <Cocoa/Cocoa.h>
+#  endif
 #endif
 
 #ifdef _WIN32
-int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrev, PSTR lpCmdLine, int nCmdShow) {
+int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrev, PSTR lpCmdLine,
+                   int nCmdShow) {
 	int C_ARGC;
 	LPWSTR *C_ARGV = CommandLineToArgvW(GetCommandLineW(), &C_ARGC);
 	if (NULL == C_ARGV) {
@@ -21,8 +36,27 @@ int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrev, PSTR lpCmdLine, int nCmdSho
 int main(int C_ARGC, const char *C_ARGV[]) {
 #endif
 
+    int ret;
+#ifdef _WIN32
+    MSG msg;
+    while (GetMessage(&msg, NULL, 0, 0)) {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+    ret = 0;
+#elif __APPLE__
+#  if TARGET_OS_IPHONE
+    @autoreleasepool {
+        ret = UIApplicationMain(C_ARGC, C_ARGV, nil,
+                                NSStringFromClass([AppDelegate class]));
+    }
+#  else 
+    ret = NSApplicationMain(C_ARGC, C_ARGV);
+#  endif
+#endif
+
 #ifdef _WIN32
 	LocalFree(C_ARGV);
 #endif
-	return 0;
+	return ret;
 }
